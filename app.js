9fac97fe1ff3b6b809d8929561f347d792c3b9e4
WaterSapphire <58876227+WaterSapphire@users.noreply.github.com>
2020-02-29 06:57:22 +0900

    新規作成

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const fs = require('fs');
+const csv = require('csv');
+
+const app = express();
+
+const infoDatafilePath = 'info-data.csv';  //情報登録用ファイル
+const fileHeader = ['Name', 'Age', 'Sex', 'PhoneNumber', 'PostalCode', 'StreetAddress'];
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
+
+app.get('/', (req, res) => {
+  res.render('top.ejs');
+});
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+app.get('/index', (req, res) => {
+  fs.readFile(infoDatafilePath, 'utf8', (err, data) => {
+    var readData = [];
+    if (err) {
+      res.render('index.ejs', {data: readData});
+    } else {
+      csv.parse(data, {columns: true}, (err, readData) => {
+        res.render('index.ejs', {data: readData});
+      });
+    }
+  });
+});
+
+app.post('/create', (req, res) => {
+  const inputData = [
+    req.body.name,
+    req.body.age,
+    req.body.sex, 
+    req.body.phoneNumber,
+    req.body.postalCode,
+    req.body.streetAddress
+  ];
+  /* ファイルの存在確認を行う */
+  fs.stat(infoDatafilePath, (err, stats) => {
+    if (err) {
+      /* ファイルが存在しない場合 */
+      /* ファイルを新規作成する */
+      fs.writeFile(infoDatafilePath, (fileHeader + "\r\n"), (err) => {});
+    }
+  });
+  /* 入力された情報をファイルに出力する */
+  fs.appendFile(infoDatafilePath, (inputData + "\r\n"), (err) => {
+    res.redirect('/index');
+  });
+});
+
+app.post('/index/delete', (req, res) => {
+  /* ファイルを削除する */
+  fs.unlink(infoDatafilePath, (err) => {
+    res.redirect('/index');
+  });
+});
+
+app.listen(3000);
